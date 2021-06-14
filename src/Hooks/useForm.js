7a3cabd92b2af8a616cbd30f35a115f5b1d7d44c
Gustavo Laranjeira <gustavolaranjeiramo@outlook.com>
2021-06-14 10:29:00 -0300

    adding regex for password

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{6,}$/,
+    message:
+      'Preencha no mínimo de 6 caracteres, pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial',
+  },
 };
 
 const useForm = (type) => {
